c96bf9383c1a1249af119dbd025a2690c710c3df
wangshengying <1600700160@qq.com>
2019-10-25 11:09:14 +0800

    作业1

---
+++ build/precache-manifest.d8f03ada4d7e3625c46c794a8f744b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42564d86dc1b07dda2dec71e82ad0bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ee8e7dc4bcdaaeeb288",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "cd6c51414d9bae98fda2",
+    "url": "/static/css/main.9076f9d3.chunk.css"
+  },
+  {
+    "revision": "4ee8e7dc4bcdaaeeb288",
+    "url": "/static/js/2.4149f7e3.chunk.js"
+  },
+  {
+    "revision": "cd6c51414d9bae98fda2",
+    "url": "/static/js/main.226f41f7.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
